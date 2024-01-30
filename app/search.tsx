7541601effe76d89c08286c230471776053617e4
agusb19 <agustinbrage19@gmail.com>
2024-01-30 10:50:14 -0300

    feat: render messages and items in search route stack

--- app/search.tsx
+++ app/search.tsx
@@ -1,10 +1,36 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, FlatList, View } from 'react-native';
+import { Text } from 'react-native-paper';
+import { StoredContext } from './_layout';
+import { useContext } from 'react';
+import { searchStocks } from '@/utils/searchStocks';
+import { StockCard } from '@/components/StockCard';
 
 export default function SearchScreen() {
-  return (
+
+  const { searchQuery, searchedStocks } = useContext(StoredContext);
+  
+  if(!searchQuery && searchStocks.length === 0) return (
     <View style={styles.container}>
- 
+      <Text variant='titleLarge' style={{ fontWeight: 'bold' }}>
+        Search Stocks
+      </Text>
     </View>
+  );
+  
+  if(searchQuery && searchStocks.length === 0) return (
+    <View style={styles.container}>
+      <Text variant='titleLarge' style={{ fontWeight: 'bold' }}>
+        No Stocks Found
+      </Text>
+    </View>
+  );
+
+  return (
+    <FlatList
+      data={searchedStocks}
+      keyExtractor={(item) => item.ticker}
+      renderItem={({ item }) => <StockCard item={item}/>}
+    />
   );
 }
 
