79dea2d8b7026a78fe467e73fbafa0182e965d3a
agusb19 <agustinbrage19@gmail.com>
2024-01-30 00:46:01 -0300

    feat: add searchStocks method to filter user searches

---
+++ utils/searchStocks.ts
@@ -0,0 +1,12 @@
+import { stocks } from '@/data';
+
+export function searchStocks(text: string) {
+    
+    if(!text) return [];
+
+    return stocks.filter(
+        (i) => 
+            i.ticker.match(new RegExp(text, 'i')) ||
+            i.companyName.match(new RegExp(text, 'i'))    
+    );
+}
