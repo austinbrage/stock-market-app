19bc2bd497b878bfbeff7f118603569831cf972e
agusb19 <agustinbrage19@gmail.com>
2024-01-30 12:29:40 -0300

    feat: add selectStock and selectStockPrices methods

--- utils/searchStocks.ts
+++ utils/searchStocks.ts
@@ -1,12 +1,22 @@
-import { stocks } from '@/data';
+import { stocks, stockPrices } from '@/data';
 
 export function searchStocks(text: string) {
-    
     if(!text) return [];
-
     return stocks.filter(
         (i) => 
             i.ticker.match(new RegExp(text, 'i')) ||
             i.companyName.match(new RegExp(text, 'i'))    
     );
+}
+
+export function selectStock(text: string) {
+    const stock = stocks.filter((i) => i.ticker === text);
+    if(stock) return stock[0];
+    return null;
+}
+
+export function selectStockPrices(text: string) {
+    const stock = stockPrices.filter((i) => i.ticker === text);
+    if(stock) return stock[0].prices;
+    return null;
 }
