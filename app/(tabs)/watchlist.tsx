4475c399556ecbff5214ae0e0d0d613aa2c7bfc5
agusb19 <agustinbrage19@gmail.com>
2024-01-30 15:48:25 -0300

    feat: render liked stocks on watchlist route tab

--- app/(tabs)/watchlist.tsx
+++ app/(tabs)/watchlist.tsx
@@ -1,9 +1,29 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, FlatList, View } from 'react-native';
+import { StockCard } from '@/components/StockCard';
+import { StoredContext } from '../_layout';
+import { Text } from 'react-native-paper';
+import { useContext } from 'react';
+import { stocks } from '@/data';
 
 export default function WatchlistScreen() {
-  return (
+
+  const { likedStocks } = useContext(StoredContext);
+
+  if(likedStocks.length === 0) return (
     <View style={styles.container}>
-      
+      <Text>No Stocks On Watchlist</Text>
+    </View>
+  );
+
+  return (
+    <View style={{ flex: 1 }}>
+      <FlatList
+        data={stocks.filter((i) => likedStocks.includes(i.ticker))}
+        keyExtractor={(item) => item.ticker}
+        renderItem={({ item }) => (
+          <StockCard item={item} />
+        )}
+      />
     </View>
   );
 }
