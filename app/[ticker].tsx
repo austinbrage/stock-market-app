c41cb2abc96a8cb48bf06a145567d4fc9448e72d
agusb19 <agustinbrage19@gmail.com>
2024-01-30 14:52:43 -0300

    feat: add description and historical metrics sections

--- app/[ticker].tsx
+++ app/[ticker].tsx
@@ -1,5 +1,7 @@
-import { Image } from 'expo-image'
-import { Text } from 'react-native-paper';
+import { Image } from 'expo-image';
+import { useState } from 'react';
+import { BarChart } from '@/components/BarChart';
+import { Text, Button } from 'react-native-paper';
 import { PriceChart } from '@/components/PriceChart';
 import { formatCurrency } from '@/utils/formatCurrency';
 import { View, SafeAreaView, Pressable, FlatList } from 'react-native';
@@ -10,9 +12,12 @@ import { selectStock, selectStockPrices } from '@/utils/searchStocks';
 export default function TickerScreen() {
 
   const { ticker } = useLocalSearchParams();
-
+  
   const stock = selectStock(ticker as string);
   const stockPrices = selectStockPrices(ticker as string);
+
+  const options = ['Description', 'Historical Metrics'];
+  const [selectedOption, setSelectedOption] = useState<string>(options[0]);
   
   return (
     <SafeAreaView style={{ flex: 1, marginHorizontal: 20, marginBottom: 10 }}>
@@ -86,6 +91,143 @@ export default function TickerScreen() {
             <View style={{ paddingTop: 20 }}>
               <PriceChart stockPrices={stockPrices}/>
             </View>
+            <FlatList
+              horizontal
+              data={options}
+              style={{ marginTop: 80 }}
+              keyExtractor={(item) => item}
+              renderItem={({ item }) => (
+                <Button
+                  style={{ marginRight: 10 }}
+                  onPress={() => setSelectedOption(item)}
+                  mode={item === selectedOption ? 'contained' : 'outlined'}  
+                  >
+                  {item}
+                </Button>
+              )}
+            />
+            {selectedOption === 'Description' ? (
+              <View style={{ marginTop: 20 }}>
+                
+                <Text 
+                  variant='titleMedium' 
+                  style={{ fontWeight: 'bold' }}
+                >
+                  CEO
+                </Text>
+                <Text>{stock.ceo}</Text>
+
+                <Text 
+                  variant='titleMedium' 
+                  style={{ fontWeight: 'bold', marginTop: 5 }}
+                >
+                  Exchange
+                </Text>
+                <Text>{stock.exchange}</Text>
+
+                <Text 
+                  variant='titleMedium' 
+                  style={{ fontWeight: 'bold', marginTop: 5 }}
+                >
+                  Sector
+                </Text>
+                <Text>{stock.sector}</Text>
+
+                <Text 
+                  variant='titleMedium' 
+                  style={{ fontWeight: 'bold', marginTop: 5 }}
+                >
+                  Industry
+                </Text>
+                <Text>{stock.industry}</Text>
+
+                <Text 
+                  variant='titleMedium' 
+                  style={{ fontWeight: 'bold', marginTop: 5 }}
+                >
+                  Location
+                </Text>
+                <Text>{stock.city}, {stock.state}</Text>
+
+                <Text 
+                  variant='titleMedium' 
+                  style={{ fontWeight: 'bold', marginTop: 5 }}
+                >
+                  IPO
+                </Text>
+                <Text>{stock.ipoDate}</Text>
+
+                <Text 
+                  variant='titleMedium' 
+                  style={{ fontWeight: 'bold', marginTop: 5 }}
+                >
+                  Description
+                </Text>
+                <Text>{stock.description}</Text>
+
+              </View>
+            ) : null} 
+
+            {selectedOption == 'Historical Metrics' ? (
+              <View style={{ marginTop: 20 }}>
+                
+                <BarChart
+                  label='ROE'
+                  color='lightblue'
+                  data={stock.returnOnEquity}
+                />
+
+                <BarChart
+                  label='ROCE'
+                  color='lightgreen'
+                  data={stock.returnOnCapitalEmployed}
+                />
+                
+                <BarChart
+                  label='Revenue'
+                  color='dodgerblue'
+                  data={stock.revenue}
+                />
+
+                <BarChart
+                  label='Earnings'
+                  color='dodgergreen'
+                  data={stock.earnings}
+                />
+
+                <BarChart
+                  label='FCF'
+                  color='maroon'
+                  data={stock.freeCashFlow}
+                />
+
+                <BarChart
+                  label='Cash'
+                  color='green'
+                  data={stock.cash}
+                />
+
+                <BarChart
+                  label='Debt'
+                  color='purple'
+                  data={stock.debt}
+                />
+
+                <BarChart
+                  label='Gross Profit Margin'
+                  color='orange'
+                  data={stock.grossProfitMargin}
+                />
+
+                <BarChart
+                  label='Net Profit Margin'
+                  color='cornSilk'
+                  data={stock.netProfitMargin}
+                />
+
+              </View>
+            ) : null}
+
           </View>
         )}
       />
